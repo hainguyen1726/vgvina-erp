89d38f333d783589c59b972ed0715e77a3422516
hainguyen1726 <hainguyen1726@gmail.com>
2026-07-14 12:13:43 +0800

    fix: resolve SyncIcon import compilation error in Admin.tsx

--- pages/Admin.tsx
+++ pages/Admin.tsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { AccountIcon, CategoryIcon, DoiTacIcon, NguoiDungIcon, HistoryIcon, SyncIcon } from '../components/icons/Icons';
+import { AccountIcon, CategoryIcon, DoiTacIcon, NguoiDungIcon, HistoryIcon } from '../components/icons/Icons';
 import { debtService } from '../src/services/debtService';
 import { useBranch } from '../contexts/BranchContext';
 import { useNotification } from '../contexts/NotificationContext';
+
+const SyncIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-full h-full">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+  </svg>
+);
 
 const Admin: React.FC = () => {
   const { currentUser } = useBranch();
